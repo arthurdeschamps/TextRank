cc39255e22f1fdd72c9cd58f0f54d85eab0a5ac7
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2020-03-24 10:44:08 +0800

    Implement PageRank (using TextRank equations) and add test for it.

--- algorithm/text_rank.py
+++ algorithm/text_rank.py
@@ -5,12 +5,12 @@ import networkx as nx
 
 class TextRank:
 
-    def __init__(self, graph: nx.Graph or nx.DiGraph, damping_factor: float):
+    def __init__(self, graph: nx.DiGraph, damping_factor: float):
         super(TextRank, self).__init__()
         self.graph = graph
         self.damping_factor = damping_factor
         self.directed = graph.is_directed()
-        self.weighted = nx.is_weighted(graph)
+        assert nx.is_weighted(graph)
 
     def run(self, delta_tol=1e-6) -> List[float]:
         self._init_nodes_scores()
@@ -18,16 +18,15 @@ class TextRank:
 
     def _run(self, it, tol):
         d_comp = 1.0 - self.damping_factor
-        if self.weighted:
-            raise NotImplementedError()
-        else:
-            for node in self.graph.nodes:
-                predecessors = self.graph.predecessors(node) if self.directed else self.graph.neighbors(node)
-                new_score = 0.0
-                for neighbour in predecessors:
-                    out_degree = self.graph.out_degree(neighbour) if self.directed else self.graph.degree(neighbour)
-                    new_score += self.graph.nodes[neighbour]['previous_score'] / out_degree
-                self.graph.nodes[node]['score'] = d_comp + self.damping_factor * new_score
+        for node in self.graph.nodes:
+            predecessors = self.graph.predecessors(node) if self.directed else self.graph.neighbors(node)
+            new_score = 0.0
+            for neighbor in predecessors:
+                successors = self.graph.successors(neighbor) if self.directed else self.graph.neighbors(neighbor)
+                edge_weight = self.graph.edges[neighbor, node]['weight']
+                sum_weights = sum(self.graph.edges[neighbor, adj_node]['weight'] for adj_node in successors)
+                new_score += edge_weight * self.graph.nodes[neighbor]['previous_score'] / sum_weights
+            self.graph.nodes[node]['score'] = d_comp + self.damping_factor * new_score
         delta = self._compute_delta()
         if delta < tol:
             self.nb_iterations = it + 1
@@ -42,10 +41,14 @@ class TextRank:
         return delta
 
     def _update_previous_scores(self):
-        for _, node in self.graph.nodes(data=True):
+        for ind, node in self.graph.nodes(data=True):
             node['previous_score'] = node['score']
+            # print(f"{ind}: {node['score']}")
 
     def _init_nodes_scores(self):
+        uniform_distr = np.full(shape=(len(self.graph.nodes),), fill_value=1/len(self.graph.nodes))
+        i = 0
         for _, node in self.graph.nodes(data=True):
-            node['score'] = np.random.uniform(low=0.0, high=10.0)
+            node['score'] = uniform_distr[i]
             node['previous_score'] = node['score']
+            i += 1
---
+++ tests/algorithm/test_text_rank.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import pytest
+
+from algorithm.text_rank import TextRank
+
+
+def test_unweighted_directed_page_rank():
+    graph = nx.DiGraph()
+    graph.add_node(0)
+    graph.add_node(1)
+    graph.add_weighted_edges_from([(0, 1, 0.5), (1, 0, 0.5), (0, 0, 0.5), (1, 1, 0.5)])
+    pr = TextRank(graph, damping_factor=1.0)
+    scores = pr.run()
+    assert (0.5 == pytest.approx(scores[0], 1e-4)) and (0.5 == pytest.approx(scores[1], 1e-4))
+
+
+def test_unweighted_undirected_page_rank():
+    graph = nx.DiGraph()
+    graph.add_nodes_from([1, 2, 3, 4])
+    for i in range(4):
+        for j in range(4):
+            if i != j:
+                graph.add_edge(i + 1, j + 1, weight=1/3.0)
+    scores = TextRank(graph, damping_factor=1.0).run()
+    for score in scores:
+        assert score == pytest.approx(0.25, 1e-4)
+
+
+def test_weighted_directed_page_rank():
+    graph = nx.DiGraph()
+    graph.add_nodes_from([1, 2])
+    a = 0.223
+    b = 0.69
+    graph.add_weighted_edges_from([
+        (1, 1, 1-a),
+        (2, 2, 1-b),
+        (2, 1, b),
+        (1, 2, a)
+    ])
+    scores = TextRank(graph, damping_factor=1.0).run()
+    assert scores[0] == pytest.approx(b/(a+b), 1e-1)
+    assert scores[1] == pytest.approx(a/(a+b), 1e-1)
